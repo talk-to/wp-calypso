f0c865878a48c4a6c85bbf8a4cd7e2bd885faa1e
Edwin Takahashi <egao@outlook.com>
2021-03-25 22:51:31 -0700

    Address Noah's code review comments. - improve the conditional logic in `.mocharc.js` where one of the mocha hook files are chosen. - restore magellan.json and magellan-playwright.json to pre-change state as the conditional hook loading in mocharc file now handles selecting the appropriate hook file. - undo changes to WebApp.kt, same reason as above.

--- test/e2e/.mocharc.js
+++ test/e2e/.mocharc.js
@@ -4,17 +4,15 @@
  */
 
 const framework = process.env.FRAMEWORK;
-let configFile;
 
 // Sets the config variable to the appropriate configuration file
 // for the specified framework.
 // As Selenium tests are the default and in the interest of not interrupting
 // existing workflow, if the value is undefined it will default to Selenium.
-if ( framework === undefined || framework.toLowerCase() === 'selenium' ) {
-	configFile = 'lib/mocha-hooks/mocha-hooks.js';
-} else if ( framework.toLowerCase() === 'playwright' ) {
-	configFile = 'lib/mocha-hooks/playwright-hooks.ts';
-}
+const configFile =
+	framework.toLowerCase() === 'playwright'
+		? 'lib/mocha-hooks/playwright-hooks.ts'
+		: 'lib/mocha-hooks/mocha-hooks.js';
 
 module.exports = {
 	require: [ 'esm', 'test/mocha.env.js', 'mocha-steps', 'ts-node/register' ],
