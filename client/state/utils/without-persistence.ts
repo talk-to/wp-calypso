00d0e658af193e663dca54bce29157d09d909f05
sarayourfriend <sarayourfriend@fastmail.com>
2021-04-12 08:20:31 -0700

    state utils: Add types to without persistence

--- client/state/utils/without-persistence.ts
+++ client/state/utils/without-persistence.ts
@@ -1,11 +1,16 @@
 /**
+ * External dependencies
+ */
+import type { Reducer } from 'redux';
+
+/**
  * Disable persistence for a reducer by removing the `serialize` and `deserialize` methods
  * from the reducer.
  *
  * @param {Function} reducer Reducer to remove persistence from
  * @returns {Function} Reducer with disabled persistence
  */
-export function withoutPersistence( reducer ) {
+export function withoutPersistence< T extends Reducer >( reducer: T ): T {
 	// disable by re-binding the reducer function
-	return reducer.bind( null );
+	return reducer.bind( null ) as T;
 }
