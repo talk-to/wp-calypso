00eace44b1929a0cd719caf7afc5b65ab81e8c32
Philip Jackson <p-jackson@live.com>
2021-04-27 16:21:56 +1200

    Add reader data store to @automattic/data-stores
    The new onboarding code doesn't use the main Calypso redux store, so it
    doesn't know whether a user is an a11n. Adding reader data to the stores
    in `@automattic/data-stores` gives it access to the reader team data,
    which is how the client can know whether a user is an a11n.
    
    This information is needed to only show the new font smoothing settings
    to a12s.

---
+++ packages/data-stores/src/reader/actions.ts
@@ -0,0 +1,17 @@
+export interface ReaderTeam {
+	slug: string;
+	team: string;
+}
+
+export interface ReaderTeamsResponse {
+	number: number;
+	teams: ReaderTeam[];
+}
+
+export const fetchReaderTeamsSuccess = ( response: ReaderTeamsResponse ) =>
+	( {
+		type: 'FETCH_READER_TEAMS_SUCCESS',
+		response,
+	} as const );
+
+export type ReaderAction = ReturnType< typeof fetchReaderTeamsSuccess >;
