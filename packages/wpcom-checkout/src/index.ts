46f07204f6e38752e67d9fbc2c36b3d2942bbe4d
Payton Swick <payton@foolord.com>
2021-04-28 18:36:31 -0400

    Add useIsWebPayAvailable

--- packages/wpcom-checkout/src/index.ts
+++ packages/wpcom-checkout/src/index.ts
@@ -12,3 +12,4 @@ export { default as Field } from './field';
 export { default as styled } from './styled';
 export * from './payment-methods/bancontact';
 export * from './payment-methods/google-pay';
+export * from './use-is-web-payment-available';
