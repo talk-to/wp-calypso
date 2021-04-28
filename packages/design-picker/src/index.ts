b6b2d670bb7de42499fe437bb9b6f45a514ffd33
Razvan Papadopol <razvan.papadopol@automattic.com>
2021-04-28 18:19:02 +0300

    Use `is_blank_canvas` site option when creating the site in New Onboarding.
    - Export isBlankCanvasDesign utility fn from 'design-picker' package

--- packages/design-picker/src/index.ts
+++ packages/design-picker/src/index.ts
@@ -3,6 +3,11 @@
  */
 export { default } from './components';
 
-export { getAvailableDesigns, availableDesignsConfig, getFontTitle } from './utils';
+export {
+	availableDesignsConfig,
+	getAvailableDesigns,
+	getFontTitle,
+	isBlankCanvasDesign,
+} from './utils';
 export { FONT_PAIRINGS, ANCHORFM_FONT_PAIRINGS, DESIGN_IMAGE_FOLDER } from './constants';
 export type { FontPair, Design } from './types';
